902882d5fd8aaf9729746002f56ee5fb0ae3a617
Yagoda020 <89180089600@mail.ru>
2023-10-07 04:55:24 +0300

    changes added

---
+++ server/service/mail-service.js
@@ -0,0 +1,13 @@
+const nodemailer = require('nodemailer');
+
+class MailService {
+
+    constructor() {
+    }
+    
+    async sendActivationMail(to, link) {
+
+    }
+}
+
+module.exports = new MailService();
